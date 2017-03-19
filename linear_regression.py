87ae58d5cb612c427d0632112882788c4aea2674
Sylvain Le Groux <sylvain.legroux@fulbrightmail.org>
2017-03-19 21:10:46 +0100

    add linear regression with multiple variables

--- linear_regression.py
+++ linear_regression.py
@@ -27,10 +27,11 @@ def least_square_error(estimate, y):
 	sum = 0
 	for i in range(len(y)):
 		sum += (estimate[i] - y[i])**2
-	return sum / (2 * len(y))
+	return sum / (2.0 * len(y))
 
 
 def mean(X):
+	X = np.array(X)
 	sum = 0
 	for v in X:
 		sum += v
@@ -38,6 +39,7 @@ def mean(X):
 
 
 def std(X):
+	X = np.array(X)
 	sum = 0
 	for v in X:
 		sum += (v - mean(X))**2
@@ -60,8 +62,9 @@ class LinearRegression(object):
 	def __init__(self):
 		# theta : model parameter of size 2
 		# J : cost history
-		self.theta = np.array([0.0, 0.0])
+		# self.theta = np.array([0.0, 0.0])
 		# self.theta = np.random.randn(2)
+		self.theta = []
 		self.J = []
 
 	def fit(self, X, y, alpha=0.01, n_iter=1000):
@@ -81,14 +84,20 @@ class LinearRegression(object):
 		"""
 		assert(len(X) == len(y)), "dimensions don't match"
 		dim = len(X)
+		X = np.array(X)
+		y = np.array(y)
+
 		# X_b: includes bias term
 		ones = np.ones(dim)
-		X_b = np.stack((ones, np.array(X)), axis=1)
+		X_b = np.column_stack((ones, np.array(X)))
+
+		# initialize weights
+		self.theta = np.random.randn(X_b.shape[1])
+
+		# iterate
 		for i in range(n_iter):
 			pred = self.predict(X)
-			# print "pred & y ", pred[:5], y[:5]
 			# gradient descent
-			# ipdb.set_trace()
 			self.theta -= float(alpha) / len(y) * np.dot((pred - y), X_b)
 			# print "theta: ", self.theta
 			self.J.append(least_square_error(pred, y))
@@ -108,10 +117,15 @@ class LinearRegression(object):
 		"""
 
 		# X: [[1,x1],..., [1,xn]], theta: [t1;t2]
-		dim = len(X)
+		X = np.array(X)
+		dim = np.shape(X)[0]
 		# include bias term in X
 		ones = np.ones(dim)
-		X_b = np.stack((ones, np.array(X)), axis=1)
+		X_b = np.column_stack((ones, np.array(X)))
+		# ipdb.set_trace()
+		if self.theta == []:
+			self.theta = np.random.randn(X_b.shape[1])
+
 		return np.dot(X_b, self.theta.T)
 
 	def save(self, X):
